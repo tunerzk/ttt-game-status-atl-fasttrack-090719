aca2e08427ff1f0d790286f1b6abd5ac000ea213
Kevin Tilghman <killjoy1100@gmail.com>
2019-09-08 18:16:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -11,7 +11,8 @@ WIN_COMBINATIONS = [
 [2,4,6]
 ]
 def #won?
- for 
+ for .each WIN_COMBINATION
+ in WIN_COMBINATIONS
 win_index_1 = WIN_COMBINATION[0]
 win_index_2 = WIN_COMBINATION[1]
 win_index_3 = WIN_COMBINATION[2]
